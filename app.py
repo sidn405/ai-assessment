0d8e685407647d213fb77fd6d8053de76f86e630
Sidney Muhammad <info@sidneym.com>
2025-12-24 02:27:17 -0600

    Updated port

--- app.py
+++ app.py
@@ -797,4 +797,4 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
